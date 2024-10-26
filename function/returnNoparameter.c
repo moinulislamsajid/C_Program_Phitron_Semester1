bd8047e3fb429b3ff0758f89976919767bca5d3c
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2024-10-26 19:44:14 +0600

    Add files via upload

---
+++ function/returnNoparameter.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+  int hello(){
+
+    int x,y,x
+
+    y = x- y;
+
+    return y;
+
+
+  }
+
+
+  int  main(){
+
+
+    int ans = hello(23,20);
+
+    printf("%d",ans);
+
+
+  }
